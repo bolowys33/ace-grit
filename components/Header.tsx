cc1edaeee3db63b0b11a1e57bffe1c2b609c8530
Olubodun <bolowys33@gmail.com>
2024-03-29 17:51:48 +0100

    Add Header to the layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,76 @@
-
+"use client";
 import Link from "next/link";
 import { links } from "@/constants/links";
-
+import logo from "@/public/assets/logo.png";
+import Image from "next/image";
+import { Container } from "@mui/material";
+import { Close, Menu } from "@mui/icons-material";
+import { useState } from "react";
+import Button from "./Button";
 
 const Header = () => {
-    return ( 
-        <header className="bg-white shadow text-emerald-950">
-            <nav className="container mx-auto flex justify-between items-center px-4 py-10">
-                <div>
-                    logo
-                </div>
-                <ul className="flex space-x-8 items-center" >
-                    {links.map(link => (
-                        <li key={link.path}>
-                            <Link href={link.path} >
-                                {link.name}
-                            </Link>
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+    const handleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <header className="bg-white shadow relative">
+            <Container maxWidth="xl">
+                <nav className="flex justify-between items-center px-4 py-4">
+                    <div>
+                        <Image src={logo} alt="brand logo" width={100} />
+                    </div>
+                    <ul className="lg:space-x-1 xl:space-x-4 hidden lg:flex items-center ">
+                        {links.map((link) => (
+                            <li key={link.path}>
+                                <Link
+                                    href={link.path}
+                                    className="link text-navy hover:border-b hover:border-navy">
+                                    {link.name}
+                                </Link>
+                            </li>
+                        ))}
+                        <li>
+                            <Button title="request an appointment" classes="ml-6 lg:ml-10" />
                         </li>
-                    ))}
-                <li>
-                <button className="bg-red-600 text-white py-2 px-4 rounded uppercase">
-                    request an appointment
-                </button>
-                </li>
-                </ul>
-            </nav>
+                    </ul>
+                    <button
+                        onClick={handleMenu}
+                        type="button"
+                        title="Menu Button"
+                        className={`p-3 bg-navy text-white rounded lg:hidden ${
+                            menuOpen ? "open" : ""
+                        }`}>
+                        <span className="transition-opacity duration-500">
+                            {menuOpen ? <Close /> : <Menu />}
+                        </span>
+                    </button>
+                </nav>
+            </Container>
+            {menuOpen ? (
+                <div
+                    className={`fixed inset-y-0 left-0 bg-opacity-50 -z-50 w-full duration-500 ${
+                        menuOpen ? "translate-y-[130px]" : "-translate-y-0"
+                    }`}>
+                    <ul>
+                        {links.map((link) => (
+                            <li key={link.path}>
+                                <div className="w-[full] text-navy border-t border-gray px-10 py-3">
+                                    <Link href={link.path} className="py-2">
+                                        {link.name}
+                                    </Link>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <div></div>
+            )}
         </header>
-     );
-}
- 
+    );
+};
+
 export default Header;
--- constants/links.ts
+++ constants/links.ts
@@ -1,11 +1,10 @@
 import { NavLink } from "@/interfaces/navlink";
 
 export const links: NavLink[] = [
-    { name: 'Home', path: '/' },
-    { name: 'About', path: '/about' },
-    { name: 'Practice Areas', path: '/practice-areas' },
-    { name: 'Attorneys', path: '/attorneys' },
-    { name: 'Legal Nuggets', path: '/legal-nuggets' },
-    { name: 'Careers', path: '/careers' },
-    { name: 'Contact', path: '/contact' },
-  ];
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Practice Areas", path: "/practice-areas" },
+    { name: "Attorneys", path: "/attorneys" },
+    { name: "Legal Blog", path: "/legal-blog" },
+    { name: "Contact", path: "/contact" },
+];
